8e86bddc8e720c5d17bc26c177cbcf5c32561abe
wuhonglie <2429986613@qq.com>
2019-05-15 08:31:53 +0000

    邮箱认证

---
+++ app/Http/Middleware/EnsureEmailIsVerified.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+
+class EnsureEmailIsVerified
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        if ($request->user() &&
+            ! $request->user()->hasVerifiedEmail() &&
+            ! $request->is('email/*', 'logout')) {
+
+            //根据客户端返回对应的内容
+            return $request->expectsJson() ? about(403, 'Your email address is not verified.')
+                                           : redirect()->route('verification.notice');
+        }
+        return $next($request);
+    }
+}
